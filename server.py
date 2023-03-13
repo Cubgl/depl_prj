6e3e9010c4b5c77566610af065a37930ae312bc0
pereskokovaoi <pereskokovaoi@gmail.com>
2023-03-13 17:27:47 +0500

    added picture

--- server.py
+++ server.py
@@ -1,13 +1,14 @@
 import os
 
-from flask import Flask
+from flask import Flask, url_for
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return "Привет от приложения Flask Teacher"
+    return f'''Привет от приложения Flask Teacher <br>
+           <img src="{url_for('static', filename='img/Колобок.png')}">'''
 
 
 if __name__ == '__main__':
